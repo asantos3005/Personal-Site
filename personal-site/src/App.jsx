2ec338db273d0a95e70cce833472d1e3fceb3af2
Andrew Santos <andrewsan321@gmail.com>
2026-06-01 16:09:53 +1000

    Laid out navbar items and basic styling.

--- personal-site/src/App.jsx
+++ personal-site/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
+import Navbar from './components/Navbar'
 import './App.css'
 
 function App() {
 
   return (
     <>
+    <Navbar/>
     </>
   )
 }
---
+++ personal-site/src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+
+
+export default function Navbar(){
+    return(
+       <nav className="bg-indigo-950 h-[50px]">
+            <ul className="flex gap-10 justify-end items-center h-full">
+                <li className="text-olive-50">
+                    About
+                </li>
+                <li className="text-olive-50">
+                    Skills
+                </li>
+                <li className="text-olive-50">
+                    Blog
+                </li>
+                <li className="text-olive-50">
+                    Projects
+                </li>
+                <li className="text-olive-50">
+                    Contact
+                </li>
+            </ul>
+       </nav> 
+    )
+}
