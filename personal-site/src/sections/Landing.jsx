ed1cd3cd6b9f083027fd278387fd3469a4ac0797
Andrew Santos <andrewsan321@gmail.com>
2026-06-06 12:52:02 +1000

    Fix anchor and arrow scroll down component on landing page- can click both the text and the arrow.

--- personal-site/src/sections/Landing.jsx
+++ personal-site/src/sections/Landing.jsx
@@ -4,6 +4,40 @@ import polygonGuy from '../assets/andrew3d.png'
 export default function Landing() {
   return (
     <section className="relative h-screen overflow-hidden bg-indigo-500">
+
+      <div className="flex flex-col justify-center items-center mt-[30vh] gap-1">
+        <h1>Welcome to Andrew's Portfolio</h1>
+        <h3 className="">Follow my journey</h3>
+        <a
+            href="#about"
+            className="body-text relative z-20 mt-5 !inline-flex flex-col items-center gap-1"
+          >
+            <span>Scroll</span>
+
+            <span className="flex h-[35px] w-[30px] items-start justify-center">
+              <motion.svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 384 512"
+                className="pointer-events-none block w-[15px]"
+                initial={{ y: 0 }}
+                animate={{ y: 12 }}
+                transition={{
+                  duration: 0.8,
+                  repeat: Infinity,
+                  repeatType: "mirror",
+                  ease: "easeInOut",
+                }}
+              >
+                <path
+                  fill="white"
+                  d="M169.4 502.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 402.7 224 32c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 370.7-105.4-105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"
+                />
+              </motion.svg>
+            </span>
+          </a>
+        
+      </div>
+      
      
       <motion.svg
         viewBox="0 0 1440 320"
