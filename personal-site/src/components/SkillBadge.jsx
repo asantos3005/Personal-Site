a54f56c01dfcafe907b6c3aa20f439d6d006f401
Andrew Santos <andrewsan321@gmail.com>
2026-06-06 11:45:11 +1000

    Added framer glow styling to skills badges and changed contact email

--- personal-site/src/components/SkillBadge.jsx
+++ personal-site/src/components/SkillBadge.jsx
@@ -1,9 +1,12 @@
+import { motion } from "motion/react";
 
 
 export default function SkillBadge(props){
     return(
-       <div className="rounded-md bg-violet-600 flex justify-center items-center min-h-[50px] min-w-[200px]">
+       <motion.div className="rounded-md bg-violet-600 flex justify-center items-center min-h-[50px] min-w-[200px]"
+       whileHover={{ scale: 1.05, boxShadow: "0 0 5px 5px rgba(255, 255, 255, 0.78)"}}
+       >
             <p className="badge-text">{props.skill}</p>
-       </div>
+       </motion.div>
     )
 }
--- personal-site/src/sections/Contact.jsx
+++ personal-site/src/sections/Contact.jsx
@@ -4,7 +4,7 @@ export default function Contact(){
     return(
         <section className="h-screen bg-indigo-950 font-mono px-8 scroll-mt-[50px]" id="contact">
             <h1 className="mb-10">Contact Me</h1>
-            <address className="block mx-auto w-fit text-4xl text-olive-50">andrewsan321@gmail.com</address>
+            <address className="block mx-auto w-fit text-4xl text-olive-50">ajrsantos77@gmail.com</address>
 
             <div className="flex justify-center gap-5 mt-3">
                 <a href="resume placeholder" className="bg-olive-50 p-5 rounded-md">My Resume</a>
